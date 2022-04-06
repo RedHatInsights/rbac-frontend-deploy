a5aa1127e7dfacd283dfe1b76df4e3bc6d33c187
Travis CI User <travis@example.org>
2022-04-06 07:16:25 +0000

    Bump @redhat-cloud-services/frontend-components from 3.7.6 to 3.8.4
    Bumps [@redhat-cloud-services/frontend-components](https://github.com/RedHatInsights/frontend-components) from 3.7.6 to 3.8.4.
    - [Release notes](https://github.com/RedHatInsights/frontend-components/releases)
    - [Commits](https://github.com/RedHatInsights/frontend-components/commits)
    
    ---
    updated-dependencies:
    - dependency-name: "@redhat-cloud-services/frontend-components"
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/348.1649228653809.38b22dc9f1236ea94762.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(96985),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/348.8fad4b1f9b5c1a789b6cb3f9bbea757f.js.map
