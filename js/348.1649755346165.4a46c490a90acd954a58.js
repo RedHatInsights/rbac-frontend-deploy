2ec9a2014be2886323ff5081c7ac13246979525c
Travis CI User <travis@example.org>
2022-04-13 06:40:49 +0000

    Bump @redhat-cloud-services/frontend-components-config-utilities
    Bumps [@redhat-cloud-services/frontend-components-config-utilities](https://github.com/RedHatInsights/frontend-components) from 1.5.19 to 1.5.20.
    - [Release notes](https://github.com/RedHatInsights/frontend-components/releases)
    - [Commits](https://github.com/RedHatInsights/frontend-components/commits)
    
    ---
    updated-dependencies:
    - dependency-name: "@redhat-cloud-services/frontend-components-config-utilities"
      dependency-type: indirect
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/348.1649755346165.4a46c490a90acd954a58.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(96985),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/348.2f61437cef9704ed522e7ebd2eec5d72.js.map
