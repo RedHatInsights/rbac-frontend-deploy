dcbc930b8cd49aea6efb683ee2707fb2b7add24a
Travis CI User <travis@example.org>
2022-04-13 18:15:40 +0000

    Move deploy template to its own folder (#1104)
    * Move deploy template to its own folder
    
    * use frontend name for file

--- js/8348.1649832528226.525e3ae04bf826a6a958.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/8348.b5190c07773b773e08f542f8d00f24a2.js.map
