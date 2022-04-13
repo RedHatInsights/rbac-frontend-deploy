01f233d78616b0fea7c5e5e46c89b774f67ff93e
Travis CI User <travis@example.org>
2022-04-13 06:49:50 +0000

    Bump @patternfly/react-tokens from 4.50.19 to 4.54.16
    Bumps [@patternfly/react-tokens](https://github.com/patternfly/patternfly-react) from 4.50.19 to 4.54.16.
    - [Release notes](https://github.com/patternfly/patternfly-react/releases)
    - [Commits](https://github.com/patternfly/patternfly-react/compare/@patternfly/react-tokens@4.50.19...@patternfly/react-tokens@4.54.16)
    
    ---
    updated-dependencies:
    - dependency-name: "@patternfly/react-tokens"
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>

--- js/8348.1649831991231.d27bc6a4ecb4461557c8.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/8348.db0e61b60d951378f224a171b6c8ed91.js.map
