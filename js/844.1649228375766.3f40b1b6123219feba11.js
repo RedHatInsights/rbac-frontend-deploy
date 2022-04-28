6564b1df2d9cae816a66a3f2d7bcff3fbc0bf345
Travis CI User <travis@example.org>
2022-04-28 11:09:49 +0000

    Merge pull request #1123 from RedHatInsights/master
    [prod-beta] Update build

--- js/844.1649228375766.3f40b1b6123219feba11.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[844],{53844:(n,t,e)=>{e.r(t),e.d(t,{default:()=>c});var r=e(96985),a=e.n(r),s=e(55140);const c=function(){var n=(0,s.Z)().quickStarts.Catalog;return a().createElement(n,null)}}}]);
