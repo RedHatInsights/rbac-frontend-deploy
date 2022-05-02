0b895d6b9cf08c0b48d4f97ecc5244644a1cd6c1
Travis CI User <travis@example.org>
2022-05-02 07:21:18 +0000

    Merge pull request #1126 from BlakeHolifield/image-fix
    Fix image name and component

--- js/8348.1651475515105.21c347d0ccfa1d70d479.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/8348.8970bc549a1c15c73099b753fcbb1f1e.js.map
