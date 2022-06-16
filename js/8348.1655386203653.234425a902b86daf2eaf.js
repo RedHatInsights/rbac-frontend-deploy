05b4d821a044b090d3ae727fd339d1964373e55f
Travis CI User <travis@example.org>
2022-06-16 13:31:07 +0000

    Merge pull request #1130 from aneelac22/feature/RHCLOUD-17856
    Update default group routing

---
+++ js/8348.1655386203653.234425a902b86daf2eaf.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=../sourcemaps/8348.450fcfac49161ddf32a3c2c25e3f9d67.js.map
