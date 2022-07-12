47a8841db66212d62b231eaa44bf2b7495283006
Travis CI User <travis@example.org>
2022-07-12 07:22:36 +0000

    Merge pull request #1189 from RedHatInsights/dismissable
    Make all Toast alerts dismissible

--- js/8348.1656688736308.57d4b2d27f6938a86fa6.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/8348.8e3799dd0271de2dd059fa175509d5e0.js.map
