6644905727a9835f79af2c5201ebd4856b240998
Travis CI User <travis@example.org>
2022-07-19 11:27:14 +0000

    Fix the props types which caused warnings (#1180)
    fix prop
    
    Fix the props types which caused warnings

---
+++ js/8348.1658229968042.6b9a84cc87c9fd3d0eb7.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=../sourcemaps/8348.0efb3f65dd1c83bd4dadd097bfc522e4.js.map
