53737b5bbe80500a44868b3cc362a7fde20dd145
Travis CI User <travis@example.org>
2022-08-23 12:31:06 +0000

    Merge pull request #1215 from fhlavac/fix-add-permissions
    Fix add permissions

--- js/3844.1661170097764.0dde7c4ca7ff1ed789bc.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[3844],{53844:(n,t,e)=>{e.r(t),e.d(t,{default:()=>c});var r=e(92950),a=e.n(r),s=e(55140);const c=function(){var n=(0,s.Z)().quickStarts.Catalog;return a().createElement(n,null)}}}]);
-//# sourceMappingURL=../sourcemaps/3844.1d02c309cfd3c90a17fd09a0ccb577a1.js.map
