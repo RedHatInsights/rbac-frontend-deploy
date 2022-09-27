fa2f9d52d686518b80de244378486ed0729998fd
Travis CI User <travis@example.org>
2022-09-27 09:01:10 +0000

    Merge pull request #1246 from fhlavac/handle
    Check cost-management access in add permissions

--- js/8348.1664266643034.7dacf0e009fc27db2de7.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/8348.5712d07426676e91b8dde4a0f0441cdc.js.map
---
+++ js/8348.1664269193122.73e935e22df1ad0264e1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=../sourcemaps/8348.fc248a52a2b601a3652d8cb6041be871.js.map
