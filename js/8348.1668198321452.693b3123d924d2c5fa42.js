c2b18d9375569bb5f553c7ff3a6fed8e682f4235
Travis CI User <travis@example.org>
2022-11-11 20:26:28 +0000

    Merge pull request #1316 from RedHatInsights/revert-1310-master
    Revert "Update master stable"

---
+++ js/8348.1668198321452.693b3123d924d2c5fa42.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=../sourcemaps/8348.124d2e43c3bf433115e713741d616477.js.map
