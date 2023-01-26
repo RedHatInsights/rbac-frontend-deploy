b502b7b427f442b079d0a901982f61bfb0fdc48b
Travis CI User <travis@example.org>
2023-01-26 16:17:46 +0000

    Merge pull request #1352 from victoria-dos/fix-wizard-escape
    Update ddf version

--- js/3844.1674476592922.17f0c80a481989fd7d62.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[3844],{53844:(n,t,e)=>{e.r(t),e.d(t,{default:()=>c});var r=e(92950),a=e.n(r),s=e(55140);const c=function(){var n=(0,s.Z)().quickStarts.Catalog;return a().createElement(n,null)}}}]);
-//# sourceMappingURL=../sourcemaps/3844.7d57099613c20cab030de0fcea52b1c8.js.map
