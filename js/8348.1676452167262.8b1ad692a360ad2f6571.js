bfa356f2d2bd83ec996a4b832f7a58031f389f5c
Travis CI User <travis@example.org>
2023-02-22 14:44:29 +0000

    Remove legacy chrome API & Main component usage (#1368)

--- js/8348.1676452167262.8b1ad692a360ad2f6571.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/8348.36f5cf0729ab0cd7aee59386333a6fef.js.map
