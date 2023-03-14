1dd1fc006f077d733bcd54903c1c018a5d6c7022
Travis CI User <travis@example.org>
2023-03-14 07:48:04 +0000

    Merge pull request #1373 from RedHatInsights/remove-connect
    Remove mapStateToProps and mapDispatchToProps

--- js/8348.fefb03b9573b40d55ca4.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/8348.916c32ed985035e31c9f63458d65a918.js.map
