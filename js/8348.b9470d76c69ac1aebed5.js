2fbdfc2eabb9600c7d126a989ddb6441db45a5c1
Travis CI User <travis@example.org>
2023-03-17 08:16:08 +0000

    Merge pull request #1377 from fhlavac/remove-old
    Remove old Add role wizard

--- js/8348.b9470d76c69ac1aebed5.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/8348.5d6358f63c5a10fe09a8d7e7a492f530.js.map
