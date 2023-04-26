5646b27ead01c4b514602badf6ad4c071c2f0c2f
Travis CI User <travis@example.org>
2023-04-26 10:39:07 +0000

    Ensure /preview is working. (#1412)
    Co-authored-by: Karel Hala <khala@redhat.com>

---
+++ js/8348.3cfe898de01ed994b394.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=../sourcemaps/8348.602d537451846fd87fbdf96c89c697e5.js.map
