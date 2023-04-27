6ec8f8f25493ec539ecec3976d0ff0ac02f6ca13
Travis CI User <travis@example.org>
2023-04-27 14:58:05 +0000

    Adjust tab titles to match new design (#1418)
    Co-authored-by: Filip Hlavac <50696716+fhlavac@users.noreply.github.com>
    Co-authored-by: Martin Maroši <marvusm.mmi@gmail.com>

---
+++ js/8348.6412cbf17b1ade3c9836.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=../sourcemaps/8348.12a6e5d461ad06c92e655b2612c9d067.js.map
