401c324f5eb7521c533905ab1017bf2b6d96021b
Travis CI User <travis@example.org>
2023-06-08 07:46:15 +0000

    Merge pull request #1376 from fhlavac/router
    Upgrade react-router to v6

---
+++ js/3844.78512e425bb1d2c0aacf.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[3844],{53844:(n,t,e)=>{e.r(t),e.d(t,{default:()=>c});var r=e(92950),a=e.n(r),s=e(55140);const c=function(){var n=(0,s.Z)().quickStarts.Catalog;return a().createElement(n,null)}}}]);
+//# sourceMappingURL=../sourcemaps/3844.a76f47f198714b55c812a3c9a064be88.js.map
