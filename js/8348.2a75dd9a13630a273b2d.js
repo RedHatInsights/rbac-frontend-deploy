bdf931a87bf417c6f80f22d6e62e316dae579023
Travis CI User <travis@example.org>
2023-07-10 13:08:18 +0000

    Merge pull request #1426 from victoria-dos/refactor-users-table
    Migrate to TS, use TableComposable for Users page (not-selectable users-list)

---
+++ js/8348.2a75dd9a13630a273b2d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=../sourcemaps/8348.1a22cf72070aa0886809ed70eb489cc8.js.map
