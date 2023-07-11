87fcf94d158a53a410162d0aa6bedf20d86f9115
Travis CI User <travis@example.org>
2023-07-11 07:34:10 +0000

    Merge pull request #1475 from fhlavac/master
    Fix an attributeFilter key for inventory

---
+++ js/8348.8be91df75bdc0476b56d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=../sourcemaps/8348.465af8ddb6cfb47077856e79e53efc73.js.map
