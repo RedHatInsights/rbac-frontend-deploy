3420a86f7e9a015e96163338806f8abf0c47353f
Travis CI User <travis@example.org>
2023-07-25 14:24:51 +0000

    Merge pull request #1492 from fhlavac/role-perms
    Fix role detail link

---
+++ js/8348.015cb0733432534296fa.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
+//# sourceMappingURL=../sourcemaps/8348.54dc0b04853a5ba588f991db451d0acc.js.map
