b99b032a65d0238f79faa9647daeddb1123a587d
Travis CI User <travis@example.org>
2023-10-04 15:11:18 +0000

    Merge pull request #1540 from fhlavac/remove-inv
    Fix inventory groups issues

--- js/8348.77de484ed8a736532c3a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_rbac_frontend=self.webpackChunkinsights_rbac_frontend||[]).push([[8348],{18348:(e,n,s)=>{s.r(n),s.d(n,{default:()=>r});var t=s(92950),i=s.n(t);const r=function(){return i().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}}]);
-//# sourceMappingURL=../sourcemaps/8348.173bc3655700c182fc5a8b80f9755d60.js.map
