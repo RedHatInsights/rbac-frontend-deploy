3a6dca1440f75a72ee1c3dad8eff710c7d06c2be
Travis CI User <travis@example.org>
2023-11-24 12:44:05 +0000

    Update SA notifications for singular and plural (#1563)

---
+++ js/5871.f5fbb1f622f11e582b18.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[5871],{65871:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var c=a(28416),r=a.n(c),n=a(55140);const s=()=>{const e=(0,n.Z)(),{quickStarts:t}=e,{Catalog:a}=t;return r().createElement(a,null)}}}]);
+//# sourceMappingURL=../sourcemaps/5871.d3f1830d5646994fcb61e68e01f8c8f9.js.map
