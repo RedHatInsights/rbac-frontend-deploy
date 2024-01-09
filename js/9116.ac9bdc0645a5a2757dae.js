2955e5826eb9481ff0c423b50307d533251506a1
Travis CI User <travis@example.org>
2024-01-09 13:57:45 +0000

    Bug fix - sorting persistance (#1574)

--- js/9116.ac9bdc0645a5a2757dae.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[9116],{9116:(e,s,c)=>{c.r(s),c.d(s,{default:()=>u});var a=c(28416),t=c.n(a);const u=()=>t().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}]);
-//# sourceMappingURL=../sourcemaps/9116.6a06df77ffb84c3f12d041020bfb2f0b.js.map
