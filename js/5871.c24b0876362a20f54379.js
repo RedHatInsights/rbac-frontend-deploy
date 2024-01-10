f09ad77134a3cacc0cef51d3bcd421fa27cbab48
Travis CI User <travis@example.org>
2024-01-10 15:23:39 +0000

    Fix loading state in group-roles (#1577)
    * Fix loading state in group-roles
    
    * Update tests

---
+++ js/5871.c24b0876362a20f54379.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[5871],{65871:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var c=a(28416),r=a.n(c),n=a(55140);const s=()=>{const e=(0,n.Z)(),{quickStarts:t}=e,{Catalog:a}=t;return r().createElement(a,null)}}}]);
+//# sourceMappingURL=../sourcemaps/5871.a7a0f80d34e9373a3da4de07d88d1a5d.js.map
