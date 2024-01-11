09ac524bb542fe8050aeecb15840ef8d102bfb4e
Travis CI User <travis@example.org>
2024-01-11 15:10:36 +0000

    Merge pull request #1578 from fhlavac/group-name
    Fix description input naming in edit group

---
+++ js/5871.edf2bd832941580d3442.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[5871],{65871:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var c=a(28416),r=a.n(c),n=a(55140);const s=()=>{const e=(0,n.Z)(),{quickStarts:t}=e,{Catalog:a}=t;return r().createElement(a,null)}}}]);
+//# sourceMappingURL=../sourcemaps/5871.ceb46c870f447f22b7e7ff73a0892fdf.js.map
