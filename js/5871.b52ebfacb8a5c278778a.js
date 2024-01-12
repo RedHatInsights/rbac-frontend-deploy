8f73ff817314b807de4ea1d66145632d525f1b29
Travis CI User <travis@example.org>
2024-01-12 13:24:50 +0000

    Fix remove service account by properly sending correct data to server (#1581)

---
+++ js/5871.b52ebfacb8a5c278778a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[5871],{65871:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var c=a(28416),r=a.n(c),n=a(55140);const s=()=>{const e=(0,n.Z)(),{quickStarts:t}=e,{Catalog:a}=t;return r().createElement(a,null)}}}]);
+//# sourceMappingURL=../sourcemaps/5871.43d89ac49b112846c8b7668683b5337a.js.map
