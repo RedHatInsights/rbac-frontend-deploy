8c578cc1639891153a928e4511abd197bbad53b4
Travis CI User <travis@example.org>
2024-01-15 07:10:12 +0000

    Filter by client ID instead of name (#1582)

--- js/9116.b52ebfacb8a5c278778a.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[9116],{9116:(e,s,c)=>{c.r(s),c.d(s,{default:()=>u});var a=c(28416),t=c.n(a);const u=()=>t().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}]);
-//# sourceMappingURL=../sourcemaps/9116.ad9720143ce5540bc2157ad673178307.js.map
