3192d40dac024ab32babacbba18427baa01812b9
Travis CI User <travis@example.org>
2024-01-25 13:20:06 +0000

    Merge pull request #1586 from radekkaluzik/RHCLOUD-30302
    Implementation of RBAC Overview page

---
+++ js/4612.3afa951b13735e95f012.js
@@ -0,0 +1,1 @@
+(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[4612],{80897:()=>{},92084:()=>{}}]);
--- js/9116.fba9e7dfe041901063df.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[9116],{9116:(e,s,c)=>{c.r(s),c.d(s,{default:()=>u});var a=c(28416),t=c.n(a);const u=()=>t().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}]);
-//# sourceMappingURL=../sourcemaps/9116.5e2d5c99d1b2de31cdf37aa1efce95c1.js.map
