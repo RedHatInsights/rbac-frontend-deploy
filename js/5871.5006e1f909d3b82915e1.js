00f4664d055cdfb2daf3f0b05a4c85f874566d47
Travis CI User <travis@example.org>
2024-02-01 15:15:51 +0000

    Merge pull request #1593 from RedHatInsights/add-desc-column
    add description column to AddService Table

---
+++ js/5871.5006e1f909d3b82915e1.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[5871],{65871:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var c=a(28416),r=a.n(c),n=a(55140);const s=()=>{const e=(0,n.Z)(),{quickStarts:t}=e,{Catalog:a}=t;return r().createElement(a,null)}}}]);
+//# sourceMappingURL=../sourcemaps/5871.be431ba21ab93c4385c37f3a1b721a3e.js.map
