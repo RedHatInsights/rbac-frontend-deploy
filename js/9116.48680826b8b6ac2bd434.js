d5094f1927954d67d5b24a8ff67ed22c5b672fe9
Travis CI User <travis@example.org>
2024-02-09 08:32:47 +0000

    remove unsubscribed section in my user access (#1598)
    * remove unsubscribed section in my user access
    
    * fix failing test

---
+++ js/9116.48680826b8b6ac2bd434.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[9116],{9116:(e,s,c)=>{c.r(s),c.d(s,{default:()=>u});var a=c(28416),t=c.n(a);const u=()=>t().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}]);
+//# sourceMappingURL=../sourcemaps/9116.afb8c9b4a66fcf947d757c0521cc6aba.js.map
