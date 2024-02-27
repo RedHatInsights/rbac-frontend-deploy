2601f275981edc6370ce0743eea88c2262082441
Travis CI User <travis@example.org>
2024-02-27 07:55:51 +0000

    Merge pull request #1601 from fhlavac/compound
    Consistency in Compound Table Views for Users and Roles

---
+++ js/5871.0114a4ba3ba5e14b5f7d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[5871],{65871:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var c=a(28416),r=a.n(c),n=a(55140);const s=()=>{const e=(0,n.Z)(),{quickStarts:t}=e,{Catalog:a}=t;return r().createElement(a,null)}}}]);
+//# sourceMappingURL=../sourcemaps/5871.3568351bc3479a5bb9542ec0a122b45a.js.map
