32cbae1992122cab910c101335c4e545b004398a
Travis CI User <travis@example.org>
2024-03-19 15:17:59 +0000

    Merge pull request #1606 from RedHatInsights/correct-ouia
    Fix ouia ids

---
+++ js/9116.df1ce12eae21dce37cbd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[9116],{9116:(e,s,c)=>{c.r(s),c.d(s,{default:()=>u});var a=c(28416),t=c.n(a);const u=()=>t().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}]);
+//# sourceMappingURL=../sourcemaps/9116.5b48307297f2de33a8b8ae63f96bbf79.js.map
