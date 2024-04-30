943514d64f90c175fdfd3a9067f4536c6e99873b
Travis CI User <travis@example.org>
2024-04-30 12:25:58 +0000

    Merge pull request #1621 from RedHatInsights/correct-buttons-ouia-tag
    fix ouia tag for buttons

---
+++ js/9116.0c19cfe6af944d14c7fc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[9116],{9116:(e,s,c)=>{c.r(s),c.d(s,{default:()=>u});var a=c(28416),t=c.n(a);const u=()=>t().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}]);
+//# sourceMappingURL=../sourcemaps/9116.afbe2b49432a9fc6ea017f44d58510ae.js.map
