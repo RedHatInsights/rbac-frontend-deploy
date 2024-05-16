47d55fa458399bb39cbcb188af62f007403c2e73
Travis CI User <travis@example.org>
2024-05-16 14:11:14 +0000

    Merge pull request #1627 from fhlavac/description
    Do not drop role description

--- js/5871.872d934e9e8b72d12b13.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[5871],{65871:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var c=a(28416),r=a.n(c),n=a(55140);const s=()=>{const e=(0,n.Z)(),{quickStarts:t}=e,{Catalog:a}=t;return r().createElement(a,null)}}}]);
-//# sourceMappingURL=../sourcemaps/5871.aeff8c70aa4b9b205fb32de08af486d1.js.map
