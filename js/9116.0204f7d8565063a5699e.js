942bb2936bb1308ccbca6055ae3a2b194459e5d1
Travis CI User <travis@example.org>
2024-05-28 13:53:50 +0000

    Merge pull request #1629 from fhlavac/upd-cg
    Update component-groups to v5.2.0-prerelease.4

---
+++ js/9116.0204f7d8565063a5699e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[9116],{9116:(e,s,c)=>{c.r(s),c.d(s,{default:()=>u});var a=c(28416),t=c.n(a);const u=()=>t().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}]);
+//# sourceMappingURL=../sourcemaps/9116.02ee3774636bf6e2ee2cad40da2c4d30.js.map
