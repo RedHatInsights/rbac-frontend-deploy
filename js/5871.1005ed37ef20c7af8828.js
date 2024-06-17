77dd4af221f7130977dbb8e5570ea0ad302e7a55
Travis CI User <travis@example.org>
2024-06-17 16:07:40 +0000

    Merge pull request #1630 from fhlavac/prev
    Fix "Back to previous page"

---
+++ js/5871.1005ed37ef20c7af8828.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[5871],{65871:(e,t,a)=>{a.r(t),a.d(t,{default:()=>s});var c=a(28416),r=a.n(c),n=a(55140);const s=()=>{const e=(0,n.Z)(),{quickStarts:t}=e,{Catalog:a}=t;return r().createElement(a,null)}}}]);
+//# sourceMappingURL=../sourcemaps/5871.13e786be6a702631b8f590e1901d2b77.js.map
