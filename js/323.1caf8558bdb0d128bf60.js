8547cd4a60624842efaf87981342e26799902894
Travis CI User <travis@example.org>
2024-06-17 13:21:37 +0000

    Merge pull request #1631 from karelhala/use-v2-rbac-client
    Use v2 RBAC client to utilize treeshaking

---
+++ js/323.1caf8558bdb0d128bf60.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[323],{52643:(e,s,a)=>{a.d(s,{b:()=>t});var l=a(65353),c=a(28416),n=a(38296),r=a(66042);const t=e=>{var{children:s=null,className:a="",component:t="div"}=e,u=(0,l.__rest)(e,["children","className","component"]);const b=t;return c.createElement(b,Object.assign({className:(0,n.css)(r.default.bullseye,a)},u),s)};t.displayName="Bullseye"},73729:(e,s,a)=>{a.r(s),a.d(s,{Bullseye:()=>l.b});var l=a(52643)}}]);
+//# sourceMappingURL=../sourcemaps/323.1621f3f474e8e5578d78a2bd8b464938.js.map
---
+++ js/9116.1caf8558bdb0d128bf60.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[9116],{9116:(e,s,c)=>{c.r(s),c.d(s,{default:()=>u});var a=c(28416),t=c.n(a);const u=()=>t().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}]);
+//# sourceMappingURL=../sourcemaps/9116.1920b36df368425c44d58ba094844004.js.map
