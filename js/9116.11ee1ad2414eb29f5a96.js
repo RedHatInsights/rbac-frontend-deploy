492438020bad8412529fad10744027353d02f8fa
Travis CI User <travis@example.org>
2024-07-15 13:39:54 +0000

    Merge pull request #1634 from fhlavac/sort
    Fix users sorting when adding them to a new group

--- js/9116.11ee1ad2414eb29f5a96.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkrbac=self.webpackChunkrbac||[]).push([[9116],{9116:(e,s,c)=>{c.r(s),c.d(s,{default:()=>u});var a=c(28416),t=c.n(a);const u=()=>t().createElement("div",{id:"rhel-mock"},"This is just a mock insights bundle module")}}]);
-//# sourceMappingURL=../sourcemaps/9116.0f781ad451a55940a671f73ba123ed12.js.map
